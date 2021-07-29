57cc70678621b19764596104feb2bdbfb24054fe
Vlad <vladivanov837@gmail.com>
2021-07-29 11:52:43 +0300

    Initial commit

--- lessons_pychr/lesson_8_1.py
+++
@@ -1,7 +0,0 @@
-'''Дан список слов. Сгенерировать новый список с перевернутыми словами'''
-
-a = 'В лесу родилась ёлочка'
-
-words = a.split()
-reversed_words = [word[::-1] for word in words]
-print(' '.join(reversed_words))
--- lessons_pychr/lesson_8_3.py
+++
@@ -1,5 +0,0 @@
-'''Дан словарь, создать новый словарь, поменяв местам
-ключ и значение'''
-old_dict = {'a' : 5, 'b' : 19, 'c' : 55}
-new_dict = {value : key for key, value in old_dict.items()}
-print(new_dict)
